3476c969083ef3e206afdfc59a8c50c3731c4d2b
mason <minh.nthcs@gmail.com>
2025-10-15 06:15:39 +0700

    generate test data using faker

--- tests/usePageObject.spec.ts
+++ tests/usePageObject.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from "@playwright/test";
 import { PageManager } from "../pages/utils/pageManager";
-
+import { faker } from "@faker-js/faker";
 test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:4200");
 });
@@ -16,6 +16,10 @@ test("navigate to Form Layout", async ({ page }) => {
 
 test("method parameterized", async ({ page }) => {
   const pm = new PageManager(page);
+  const randomFullName = faker.person.fullName();
+  const randomEmail = `${randomFullName.replace(" ", "")}${faker.number.int(
+    1000
+  )}@test.com`;
   await pm.navigateTo().FormLayoutsPage();
   await pm
     .onFormLayoutsPage()
@@ -26,9 +30,9 @@ test("method parameterized", async ({ page }) => {
     );
   await pm
     .onFormLayoutsPage()
-    .inputFormUsingInlineForm("Minh", "masson@gmail.com", true);
+    .inputFormUsingInlineForm(randomFullName, randomEmail, true);
 
-  await pm.navigateTo().datePickerPage();
-  await pm.onDatePickerPage().selectCommonDatePickerDateFromToday(6);
-  await pm.onDatePickerPage().selectDatePickerWithRangeFromToday(4, 23);
+  // await pm.navigateTo().datePickerPage();
+  // await pm.onDatePickerPage().selectCommonDatePickerDateFromToday(6);
+  // await pm.onDatePickerPage().selectDatePickerWithRangeFromToday(4, 23);
 });
